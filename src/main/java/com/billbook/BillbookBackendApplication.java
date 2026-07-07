94ebd482ed05be835b2a891604f707843d42de5a
ShirinCoder <shirinengineer2@gmail.com>
2026-07-07 12:39:39 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/billbook/BillbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.billbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillbookBackendApplication.class, args);
+	}
+
+}
